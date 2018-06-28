f43119fe3b327df75bc74bd62c3012b636524b85
trungdao912 <40337350+trungdao912@users.noreply.github.com>
2018-06-29 00:57:32 +0700

    Update printout1and0table_2.py

--- Session02/homework/printout1and0table_2.py
+++ Session02/homework/printout1and0table_2.py
@@ -1,26 +1,24 @@
 n = int(input("enter your number:"))
 
+a = 1
+b = 0
+
 if n % 2 == 0:
     for k in range(n // 2):
         for i in range(n // 2):
-            a = 1
-            b = 0
             print(a, b, end = " ")
         print()
         for j in range(n // 2):
-            a = 1
-            b = 0
             print(b, a, end = " ")
         print()
 else:
     for k in range(n // 2):
         for i in range(n // 2):
-            a = 1
-            b = 0
             print(a, b, end = " ")
         print(1)
         for j in range(n // 2):
-            a = 1
-            b = 0
             print(b, a, end = " ")
         print(0)
+    for m in range(n // 2):
+        print(a, b, end = " ")
+    print(1)
