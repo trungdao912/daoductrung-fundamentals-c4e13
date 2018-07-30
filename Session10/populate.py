0e0765a2360e7e525d9aef4b85fc96d6362b5770
abc <trungdao912@gmail.com>
2018-07-30 21:32:56 +0700

    hw

--- Session10/populate.py
+++ Session10/populate.py
@@ -4,20 +4,7 @@ from faker import Faker
 from random import randint, choice
 
 mlab.connect()
-
 fake = Faker()
-# for i in range(50):
-#     print("Saving service", i+1,"......")
-#     new_service = Service(
-#         name = fake.name(),
-#         yob = randint(1990, 2000),
-#         gender = randint(0,1),
-#         height = randint(150, 190),
-#         phone = fake.phone_number(),
-#         address = fake.address(),
-#         status = choice([True, False])
-#     )
-#     new_service.save()
 
 for i in range(50):
     print(i + 1)
